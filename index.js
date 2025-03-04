262aef4afa65f37a9148d4b8394a29c9801c5b8f
farzeen-ali <farzeenaliarif786@gmail.com>
2025-03-04 23:31:35 +0500

    Create Docker Image with Node App

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+
+const app = express();
+
+app.get("/", (req, res)=> {
+    res.json(
+        [
+            {
+                id: 1,
+                employeeName: "Farzeen",
+                employeeSalary: 70000
+            },
+            {
+                id: 2,
+                employeeName: "Ali",
+                employeeSalary: 60000
+            },
+            {
+                id: 3,
+                employeeName: "Huzaifa",
+                employeeSalary: 10000
+            },
+        ]
+    )
+})
+
+app.listen(4000, () => {
+    console.log("App is running on Port No: 4000")
+})
